53c23f29894cb81a161394751f3491dd22a27998
Loan <loanbillonnet@gmail.Com>
2023-03-22 15:54:23 +0100

    deploy project

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/person')
+
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT
 
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
@@ -44,7 +48,11 @@ const generatedId = () => {
 
 
 // ROUTES
-app.get('/api/persons', (request, response) => response.json(persons))
+app.get('/api/persons', (request, response) => {
+    Person.find({})
+        .then(result => result.forEach(person => response.json(person)))
+        .catch(e => response.json({error: e}).status(500).end())
+})
 app.get('/api/info', (request, response) => {
     currentDate = new Date()
     response.send(`
@@ -54,37 +62,30 @@ app.get('/api/info', (request, response) => {
 })
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
     
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(id)
+        .then(person => response.json(person))
+        .catch(e => response.json({error: e}).status(404).end())
 })
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
 
-    response.status(204).end()
-})
+//     persons = persons.filter(person => person.id !== id)
+
+//     response.status(204).end()
+// })
 app.post('/api/persons', (request, response) => {
     const body = request.body
     
     if (!body.name || !body.number)
         return response.status(400).json({error: 'name or number is missing'})
 
-    if (persons.find(person => person.name === body.name))
-        return response.status(400).json({error: 'name already used'})
-    
-    const person = {
-        id: generatedId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => response.json(savedPerson))
 })
 
 
